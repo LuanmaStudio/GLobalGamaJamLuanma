b93769efdf7aeb66b8b00903d1e80ae9434a6dce
EnderHorror <36545996+EnderHorror@users.noreply.github.com>
2021-01-29 22:51:11 +0800

    MoreFunction

---
+++ Assets/Scripts/ShadowPool.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowPool : MonoBehaviour
+{
+    public static ShadowPool instance;
+
+    public GameObject shadowPrefab;
+
+    public int shadowCount;
+
+    private Queue<GameObject> availableObjects = new Queue<GameObject>();
+
+    void Awake()
+    {
+        instance = this;
+
+        //初始化对象池
+        FillPool();
+    }
+
+    public void FillPool()
+    {
+        for (int i = 0; i < shadowCount; i++)
+        {
+            var newShadow = Instantiate(shadowPrefab);
+            newShadow.transform.SetParent(transform);
+
+            //取消启用,返回对象池
+            ReturnPool(newShadow);
+        }
+    }
+
+    public void ReturnPool(GameObject gameObject)
+    {
+        gameObject.SetActive(false);
+
+        availableObjects.Enqueue(gameObject);
+    }
+
+    public GameObject GetFormPool()
+    {
+        if (availableObjects.Count == 0)
+        {
+            FillPool();
+        }
+        var outShadow = availableObjects.Dequeue();
+
+        outShadow.SetActive(true);
+
+        return outShadow;
+    }
+}
---
+++ Assets/Scripts/WordsAnalyze.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Text.RegularExpressions;
+using System.Text;
+
+/// <summary>
+/// 记号
+/// </summary>
+public struct Mark
+{
+    public int Key;
+    public string Value;
+
+    public Mark(int key,string value)
+    {
+        Key = key;
+        Value = value;
+    }
+        
+
+}
+/// <summary>
+/// 词法分析
+/// </summary>
+public class WordsAnalyze
+{
+    /// <summary>
+    /// 读取Token流
+    /// </summary>
+    /// <param name="content"></param>
+    /// <returns>要读取的字符串</returns>
+    public static Dictionary<int, string> ReadToken(string content)
+    {
+        var mark = Regex.Matches(content, @"{|}");
+        Dictionary<int, string> token = new Dictionary<int, string>();
+        Stack<int> markStack = new Stack<int>();
+
+        for (int i = 0; i < mark.Count; i++)
+        {
+            if (mark[i].Value == "{")
+            {
+                markStack.Push(mark[i].Index);
+            }
+            else if (mark[i].Value == "}")
+            {
+                int start = markStack.Pop();
+                string buffer = content.Substring(start, mark[i].Index - start+1);
+                token.Add(start, buffer);
+            }
+        }
+        
+
+        return token;
+    }
+
+    /// <summary>
+    /// 把KeyWords去掉
+    /// </summary>
+    /// <param name="text">需要除关键字的字符串</param>
+    /// <param name="mark">返回的标记位置以及关键词</param>
+    /// <returns>除去关键字的结果</returns>
+    public static string RemoveKeyWords(string text,out List<Mark> mark)
+    {
+        string tempString = text;
+        var token = ReadToken(text);
+        mark = new List<Mark>();
+        while (token.Count!=0)
+        {
+            token = ReadToken(tempString);
+            foreach (var item in token)
+            {
+                tempString = tempString.Remove(item.Key, item.Value.Length);
+                mark.Add(new Mark(item.Key,item.Value.Substring(1, item.Value.Length-2)));
+                break;
+            }
+        }
+
+        return tempString;
+    }
+    /// <summary>
+    /// 把文本文件的字符切分成一行一行的
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    public static string[] ClipLine(string text)
+    {
+        return Regex.Split(text, @"\n");
+    }
+}
