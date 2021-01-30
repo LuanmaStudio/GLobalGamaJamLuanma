6e4c26a9a92e95bf0eed887093879126abae1f02
insyent <1049966282@qq.com>
2021-01-30 16:48:46 +0800

    UI框架 差音量条和MainPanel

---
+++ Assets/Scripts/SceneManager/SceneSystem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// 场景状态管理
+/// </summary>
+public class SceneSystem
+{
+    /// <summary>
+    /// 场景状态类
+    /// </summary>
+    private SceneState sceneState;
+
+    /// <summary>
+    /// 设置当前场景并进入当前场景
+    /// </summary>
+    /// <param name="state"></param>
+    public void SetScene(SceneState state)
+    {
+        sceneState?.OnExit();
+        sceneState = state;
+        sceneState?.OnEnter();
+    }
+}
---
+++ Assets/Scripts/UIFrame/Concrete/ChangePanel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangePanel : BasePanel
+{
+    private static readonly string path = "prefabs/UI/Panel/ChangePanel";
+    
+    public ChangePanel() : base(new UIType(path)){ }
+
+    private static bool isW = false;
+    private static bool isA = false;
+    private static bool isD = false;
+
+
+    public override void OnEnter()
+    {
+        if (isW == false)
+        {
+            UITool.FindChildGameObject("Jump").transform.Find("Key").GetComponent<Text>().text = "&%#";
+        }
+        if (isA == false)
+        {
+            UITool.FindChildGameObject("Left").transform.Find("Key").GetComponent<Text>().text = "!*#";
+        }
+        if (isD == false)
+        {
+            UITool.FindChildGameObject("Right").transform.Find("Key").GetComponent<Text>().text = "$^&";
+        }
+        UITool.GetOrAddComponentInChildren<Button>("Exit").onClick.AddListener(() =>
+        {
+            //点击事件
+            PanelManager.Pop(); 
+        });
+        UITool.GetOrAddComponentInChildren<Button>("Jump").onClick.AddListener(() =>
+        {
+            //点击事件
+            UITool.FindChildGameObject("Jump").transform.Find("Key").GetComponent<Text>().text = "Space";
+            isW = true;
+        });
+        UITool.GetOrAddComponentInChildren<Button>("Left").onClick.AddListener(() =>
+        {
+            //点击事件
+            UITool.FindChildGameObject("Left").transform.Find("Key").GetComponent<Text>().text = "A";
+            isA = true;
+        });
+        UITool.GetOrAddComponentInChildren<Button>("Right").onClick.AddListener(() =>
+        {
+            //点击事件
+            UITool.FindChildGameObject("Right").transform.Find("Key").GetComponent<Text>().text = "D";
+            isD = true;
+        });
+    }
+    
+}
